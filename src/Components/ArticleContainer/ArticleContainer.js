4cd975ab0712e48cff05ab60fef13235cde86f5c
Derek Yeh <derek.yeh91@gmail.com>
2023-04-20 00:38:22 -0700

    Use async await for network request

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,78 +1,59 @@
 import React, { useEffect, useState } from "react";
 import SingleArticle from '../SingleArticle/SingleArticle'
 import './ArticleContainer.css'
-import {mockData} from "../../mockData"
 
 const ArticleContainer = ({setCurrentArticle}) => {
-    console.log(mockData)
 
-    const topStories = mockData.results
-
-    // const [topStories, setTopStories] = useState()
+    const [topStories, setTopStories] = useState()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(true)
+    const [filter, setFilter] = useState('')
 
-    // const getStories = () => {
-    //     return fetch(""
-    //         'https://c535730c-b76d-4a7f-a6f6-2d101629a2c4.mock.pstmn.io', {
-    //         method: "GET",
-    //         headers: {
-    //             "api-key": "G0CtJOsSP065KA2Nzd4tz0wIMec1Sgv1"
-    //         }}
-    //     )
-    //     .then(res => {
-    //         if(res.ok) {
-    //             return res.json()
-    //         } else {
-    //             throw new Error()
-    //         }
-    //     })
-    //     .catch(err => {
-    //         setError(err)
-    //     })
-    // }
+    const handleFilter = (e) => {
+        setFilter(e.target.value)
+    }
 
-    // useEffect(() => {
-    //     setTopStories(mockData.results)
-    //     // if(loading){
-    //     //     setLoading(false)
-    //     //     console.log(topStories)
-    //     //     getStories()
-    //     //     .then(data => {
-    //     //         setTopStories(data)
-    //     //     })
-    //     // } 
-    // },[topStories])
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(`https://api.nytimes.com/svc/${filter}/v2/home.json?api-key=G0CtJOsSP065KA2Nzd4tz0wIMec1Sgv1`)
+            const data = await res.json()
+            .catch((error) => {
+                setError(error)
+            })
+            setTopStories(data)
+            setLoading(false)
+        }
+        fetchData();
+    },[filter])
 
-    // The possible section value are: arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, and world.
-    // pass in to use in fetch request?
-
-    const stories = topStories.map((story) => {
-        return <SingleArticle 
-        setArticle={setCurrentArticle}
-        story={story}
-        key={story.short_url}
-        shortURL={story.short_url}
-        section={story.section}
-        subsection={story.subsection}
-        title={story.title}
-        url={story.url}
-        created_date={story.created_date}/>
-    })
-    
-
-    return (
-        <section className="main-container">
-            <section className="top-main-container">
-                TOP STORIES
-            </section>
-            <section className="bottom-container">
-                <section className="top-stories-container">
-                    {stories}
+    if (!loading) {
+        return (
+            <section className="main-container">
+                <section className="top-main-container">
+                    TOP STORIES
+                    <button onClick={handleFilter} value='topstories'>Top Stories</button>
+                    <button onClick={handleFilter} value='home'>Home</button>
+                </section>
+                <section className="bottom-container">
+                    <section className="top-stories-container">
+                        {topStories.map((story) => {
+                            return <SingleArticle 
+                            setArticle={setCurrentArticle}
+                            story={story}
+                            key={story.short_url}
+                            shortURL={story.short_url}
+                            section={story.section}
+                            subsection={story.subsection}
+                            title={story.title}
+                            url={story.url}
+                            created_date={story.created_date}/>
+                        })}
+                    </section>
                 </section>
             </section>
-        </section>
-    )
+        )
+    }
+
 }
 
 export default ArticleContainer;
